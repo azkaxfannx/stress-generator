e4fb6ea86d9eb2b5b5c0f3a401bd12191416001e
azkaxfannx <74809660+azkaxfannx@users.noreply.github.com>
2023-11-07 22:10:23 +0700

    Update backend.js

--- backend.js
+++ backend.js
@@ -1,101 +1,102 @@
-var btn1 = document.getElementById('btn1');
-var btn2 = document.getElementById('btn2');
-var btn3 = document.getElementById('btn3');
-var btn4 = document.getElementById('btn4');
-var btn5 = document.getElementById('btn5');
-var btn6 = document.getElementById('btn6');
-var btn7 = document.getElementById('btn7');
-var btn8 = document.getElementById('btn8');
-var btn9 = document.getElementById('btn9');
-var btn10 = document.getElementById('btn10');
-var btn11 = document.getElementById('btn11');
-var btn12 = document.getElementById('btn12');
-var currentBotton = 1;
+const btn1 = document.getElementById('btn1');
+const btn2 = document.getElementById('btn2');
+const btn3 = document.getElementById('btn3');
+const btn4 = document.getElementById('btn4');
+const btn5 = document.getElementById('btn5');
+const btn6 = document.getElementById('btn6');
+const btn7 = document.getElementById('btn7');
+const btn8 = document.getElementById('btn8');
+const btn9 = document.getElementById('btn9');
+const btn10 = document.getElementById('btn10');
+const btn11 = document.getElementById('btn11');
+const btn12 = document.getElementById('btn12');
+let currentBotton = 1;
+
 
 function inputan() {
     return document.getElementById('namaInput').value.toUpperCase();
 }
 
 function oncBtn1() {
-    var namaWaifu = inputan();
-    var hasilWaifu = document.getElementById('hasilWaifu');
+    let namaWaifu = inputan();
+    let hasilWaifu = document.getElementById('hasilWaifu');
     hasilWaifu.innerHTML = `${namaWaifu} ${namaWaifu} ${namaWaifu} ❤️ ❤️ ❤️ WANGI WANGI WANGI WANGI HU HA HU HA HU HA, aaaah baunya rambut ${namaWaifu} wangi aku mau nyiumin aroma wanginya ${namaWaifu} AAAAAAAAH ~ Rambutnya.... aaah rambutnya juga pengen aku elus-elus ~~~~ AAAAAH ${namaWaifu} keluar pertama kali di anime juga manis ❤️ ❤️ ❤️ banget AAAAAAAAH ${namaWaifu} AAAAA LUCCUUUUUUUUUUUUUUU............ ${namaWaifu} AAAAAAAAAAAAAAAAAAAAGH ❤️ ❤️ ❤️ apa ? ${namaWaifu} itu gak nyata ? Cuma HALU katamu ? nggak, ngak ngak ngak ngak NGAAAAAAAAK GUA GAK PERCAYA ITU DIA NYATA NGAAAAAAAAAAAAAAAAAK PEDULI BANGSAAAAAT !! GUA GAK PEDULI SAMA KENYATAAN POKOKNYA GAK PEDULI. ❤️ ❤️ ❤️ ${namaWaifu} gw ...${namaWaifu} di laptop ngeliatin gw, ${namaWaifu} .. kamu percaya sama aku ? aaaaaaaaaaah syukur ${namaWaifu} aku gak mau merelakan ${namaWaifu} aaaaaah ❤️ ❤️ ❤️ YEAAAAAAAAAAAH GUA MASIH PUNYA ${namaWaifu} SENDIRI PUN NGGAK SAMA AAAAAAAAAAAAAAH`;
 }
 
 function oncBtn2() {
-    var namaWaifu = inputan();
+    let namaWaifu = inputan();
     
-    var hasilWaifu = document.getElementById('hasilWaifu');
+    let hasilWaifu = document.getElementById('hasilWaifu');
     hasilWaifu.innerHTML = `BUKA BAJU ${namaWaifu} LANGSUNG CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CROT CEPLOK CEPLOK CEPLOK CEPLOK CROT CROT CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CROT CEPLOK CEPLOK CEPLOK CROT CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CROT CEPLOK CEPLOK CEPLOK CEPLOK CROTCEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CROT CEPLOK CEPLOK CEPLOK CEPLOK CROT CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CROT CEPLOK CEPLOK CEPLOK CEPLOK CROTCEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CROT CEPLOK CEPLOK CEPLOK CEPLOK CROT MONNCROOOTOOOOOOOOOOOOOOOOOOOOOOO`;
 }
 
 function oncBtn3() {
-    var namaWaifu = inputan();
+    let namaWaifu = inputan();
     
-    var hasilWaifu = document.getElementById('hasilWaifu');
+    let hasilWaifu = document.getElementById('hasilWaifu');
     hasilWaifu.innerHTML = `Buruan, panggil gue SIMP, ato BAPERAN. ini MURNI PERASAAN GUE. Gue pengen genjot bareng ${namaWaifu}. Ini seriusan, suaranya yang imut, mukanya yang cantik, apalagi badannya yang aduhai ningkatin gairah gue buat genjot ${namaWaifu}. Setiap lapisan kulitnya pengen gue jilat. Saat gue mau crot, gue bakal moncrot sepenuh hati, bisa di perut, muka, badan, teteknya, sampai lubang burit pun bakal gue crot sampai puncak klimaks. Gue bakal meluk dia abis gue moncrot, lalu nanya gimana kabarnya, ngrasain enggas bareng saat telanjang. Dia bakal bilang kalau genjotan gue mantep dan nyatain perasaannya ke gue, bilang kalo dia cinta ama gue. Gue bakal bilang balik seberapa gue cinta ama dia, dan dia bakal kecup gue di pipi. Terus kita ganti pakaian dan ngabisin waktu nonton film, sambil pelukan ama makan hidangan favorit. Gue mau ${namaWaifu} jadi pacar, pasangan, istri, dan idup gue. Gue cinta dia dan ingin dia jadi bagian tubuh gue. Lo kira ini copypasta? Kagak cok. Gue ngetik tiap kata nyatain prasaan gue. Setiap kali elo nanya dia siapa, denger ini baik-baik : DIA ISTRI GUE. Gue sayang ${namaWaifu}, dan INI MURNI PIKIRAN DAN PERASAAN GUE.`;
 }
 
 function oncBtn4() {
-    var namaWaifu = inputan();
+    let namaWaifu = inputan();
     
-    var hasilWaifu = document.getElementById('hasilWaifu');
+    let hasilWaifu = document.getElementById('hasilWaifu');
     hasilWaifu.innerHTML = `AHHHHH TETEK ${namaWaifu} GEDE BANGETTT AHHH ISEP ISEP ISEP SEDOT SEDOT CROOTT AHH SUSUNYAAA MONCROOT KEMANA MANAA CROOT KEMUKAA AA ISEP ISEP ISEP SEDOTT CROTTT ${namaWaifu} AHHH`;
 }
 
 function oncBtn5() {
-    var namaWaifu = inputan();
+    let namaWaifu = inputan();
     
-    var hasilWaifu = document.getElementById('hasilWaifu');
+    let hasilWaifu = document.getElementById('hasilWaifu');
     hasilWaifu.innerHTML = `GW BENAR-BENAR PENGEN JILATI KAKI ${namaWaifu} GW PENGEN BANGET MENJILAT SETIAP BAGIAN KAKINYA SAMPAI AIR LIUR GW BERCUCURAN KAYAK AIR KERINGAT LALU NGENTOD DENGANNYA SETIAP HARI SAMPAI TUBUH KITA MATI RASA YA TÜHAN. GW INGIN MEMBUAT ANAK-ANAK DENGAN ${namaWaifu} SEBANYAK SATU TÌM SEPAK BOLA DAN MEMBUAT SATU TIM SEPAK BOLA LAINNYA UNTUK MELAWAN ANAK-ANAK TIM SEPAK BOLA PERTAMA GW YANG GW BUAT SAMA ${namaWaifu} GW PENGEN MASUK KE SETIAP LUBANG TUBUHNYA, MAU ITU LUBANG HIDUNG, LUBANG TELINGA, RONGGA MATA MAUPUN PUSAR, KECUALI PORI-PORI KULIT. KEMUDIAN GW AKAN MENJADIKANNYA MANUSIA YANG TIDAK BISA HIDUP KALO TIDAK GW KENTOD SETIAP HARI. DAN KALAU GUA DISEPONG GUA RELA KONTOL GUA PUTUS.`;
 }
 
 function oncBtn6() {
-    var namaWaifu = inputan();
+    let namaWaifu = inputan();
     
-    var hasilWaifu = document.getElementById('hasilWaifu');
+    let hasilWaifu = document.getElementById('hasilWaifu');
     hasilWaifu.innerHTML = `MAKSUD LO APA HINA2 ${namaWaifu} HAH?!!!! GUE MESTI KASIHTAU YA ${namaWaifu} ITU RAJANYA MUSIK!!! ${namaWaifu} UDAH MENGUNJUNGI NOMINASI2 TERATAS, DAN PUNYA LEBIH 200 PENGHARGAAN!! ${namaWaifu} UDAH TERLATIH SENI MENARI DAN ${namaWaifu} ITU TERBAIK DI SEGALA INDUSTRI TARI MENARI. MUKA LO GAADA APA2NYA DI BANDING SAMA ${namaWaifu} GUA ARMY BAKAL INCAR LO SAMPE GAADA DI MUKA BUMI INI!! CAMKAN KATA2 ARMY!!! LU PIKIR BISA SEENAK NYA NGATAIN ${namaWaifu} GUA BEGITU AJA ???? PIKIR ULANG NJING!!! PAS GUE NGETIK INI GUA UDAH HUBUNGIN DAN MEMANGGIL SELURUH ARMY DI SEKOJUR DUNIA DAN MUKA LU UDAH GUA LACAK BUAT UU ITE!! ARMY ADA DI MANA MANA DAN KAPAN AJA BISA BULLY ELO DENGAN SEGALA CARA NJING!!! COBA AJA LO GA NGEHINA ${namaWaifu} GW ATAU LO BISA JAGA MULUT. LO GABISA KAN?? ARMY BAKAL HANCUR SAMA KAWAN2 LO YANG LAIN YANG HINA ${namaWaifu}. LU BAKAL MATI NJING!!!! CAMKAN!!!!!`;
 }
 
 function oncBtn7() {
-    var namaWaifu = inputan();
+    let namaWaifu = inputan();
     
-    var hasilWaifu = document.getElementById('hasilWaifu');
+    let hasilWaifu = document.getElementById('hasilWaifu');
     hasilWaifu.innerHTML = `${namaWaifu} ${namaWaifu} ${namaWaifu} ${namaWaifu} ${namaWaifu} ${namaWaifu} AYO KITA MENIKAH ${namaWaifu}-KU SAYANG.AKU AKAN MENCARI BUAH AMBROSIA UNTUK KITA BERDUA SUPAYA KITA HIDUP ABADI, LALU SETELAH ITU AKU AKAN MENABUR BENIH DI DALAMMU HINGGA VOLUMENYA MENCAPAI SESAMUDERA HINDIA, KEMUDIAN ANAK KITA AKAN BERLIMPAH SEJUMLAH POPULASI MANUSIA DI PLANET BUMI. KEMUDIAN SETELAH POPULASI MANUSIA TERLALU BANYAK, AKU AKAN MEN-TERRAFORM PLANET ${namaWaifu} SUPAYA LAYAK HUNI, DAN KITA SERTA SEBAGIAN ANAK KITA AKAN PINDAH KE SANA, MENJADI PENGHUNI PLANET ${namaWaifu}. KEMUDIAN AKU AKAN MENABUR BENIH LAGI DI DALAMMU HINGGA VOLUMENYA MENCAPAI SEGUNUNG OLYMPUS MONS, KEMUDIAN AKAN LAHIR BERLIMPAH ANAK BARU KITA SEJUMLAH POPULASI ANAK KITA YANG IKUT PINDAH KE ${namaWaifu}, KEMUDIAN SETELAH POPULASI MANUSIA DI ${namaWaifu} TERLALU BANYAK, AKU AKAN MEN-TERRAFORM PLANET JUPITER SUPAYA LAYAK HUNI, KEMUDIAN AKAN MENABUR BENIH DALAMMU LAGI, MELAHIRKAN MILYARAN ANAK LAGI, PINDAH KE PLANET YANG SUDAH DIBUAT LAYAK HUNI, DAN PROSES ITU TERUS DIULANG HINGGA KETURUNAN KITA MENGHUNI SELURUH PLANET DI GALAKSI BIMA SAKTI DAN KITA AKAN MENGUASAI GALAKSI ITU, KEMUDIAN MENYATAKAN PERANG DENGAN MAKHLUK DARI GALAKSI LAIN, MENGALAHKAN MEREKA, DAN MENGAMBIL ALIH GALAKSI MEREKA UNTUK MEMPERLUAS WILAYAH KITA SAMPAI KITA BERDUA MENGUASAI ALAM SEMESTA.SUDAH SAATNYA BAGI KETURUNANKU DAN GURA UNTUK MENEMPATI SELURUH BAGIAN ALAM SEMESTA`;
 }
 
 function oncBtn8() {
-    var namaWaifu = inputan();
+    let namaWaifu = inputan();
     
-    var hasilWaifu = document.getElementById('hasilWaifu');
+    let hasilWaifu = document.getElementById('hasilWaifu');
     hasilWaifu.innerHTML = `apa-apaan yang barusan ${namaWaifu} bilang itu jing? ${namaWaifu} harus tau kalo gue itu adalah agen warung Indomie terbaik, dan gue udah beberapa kali terlibat dalam serangan rahasia ke pabrik mie Nissin di Jepang, dan gue udah berhasil menewaskan lebih dari 300 sales Mie Sedaap. Gue terlatih dalam peperangan gorilla dan gue adalah mamang Warmindo terbaik se-Karawang. ${namaWaifu} itu ga berarti apa-apa buat gue, buat gue ${namaWaifu} itu cuma target biasa. Bener-bener gue lenyapkan ${namaWaifu} dari dunia ini, inget kata-kata gue. ${namaWaifu} pikir ${namaWaifu} bisa kabur dari bacotan ${namaWaifu} di internet? Pikir lagi jing. Gue barusan aja kontak sama jaringan mata-mata rahasia gue di seluruh Indonesia dan IP ${namaWaifu} udah gue telusuri jadi sekarang ${namaWaifu} mendingan siap-siap jing. Bakal gue abisin idup ${namaWaifu} yang ngenes itu. Mati ${namaWaifu} jing. Gue bisa ada dimana aja, kapan aja, dan gue bisa ngebunuh ${namaWaifu} lebih dari tujuh ratus cara, semuanya pake tangan kosong. Gak cuma terlatih di pertarungan tanpa senjata, tapi gue juga punya akses ke seluruh gudang distributor Indomie dan bakal gue pake semaksimal mungkin buat ngehapus tampang burik ${namaWaifu} dari muka bumi ini, dasar ampas. Kalo aja ${namaWaifu} tau bencana apa yang bakal nimpa ${namaWaifu} karena komen sok pinter itu, mungkin ${namaWaifu} bakal ngejaga mulut ${namaWaifu} yang bau ketek itu. Tapi ${namaWaifu} gak bisa, dan ${namaWaifu} tetep bacot, dan sekarang ${namaWaifu} harus terima akibat dari kegoblokan ${namaWaifu}. ${namaWaifu} bakal abis kebakar api emosi gue. Mampus ${namaWaifu} jing.`;
 }
 
 function oncBtn9() {
-    var namaWaifu = inputan();
+    let namaWaifu = inputan();
     
-    var hasilWaifu = document.getElementById('hasilWaifu');
+    let hasilWaifu = document.getElementById('hasilWaifu');
     hasilWaifu.innerHTML = `Please God, I want to impregnate ${namaWaifu} so dang bad. I want her to bear my children with those busty yet sleek and pretty thighs. That beautiful almost eye sparkling angel. Like a goddess, having come down to Earth to cleanse all of our sins. ${namaWaifu} is beyond perfect. I can't help but drop to my knees in worship whenever I see her beautiful figure. Just thinking about staring at her as she notices makes my heart thump in an unexpected way.I yearn for her in a way. both primal and spiritual. I would commit more war crimes than every president in United States history and every reoccurring crime in those Spider-Man games or even as many grains of sand there are in the world just to lick the sweet, glistening sweat from her smooth, creamy skin. I want to listen to her moans as my manhood throbs within her, I want to hear her heart race as our bodies become one and our souls irreversibly intertwine in the holy sin of carnal union. I love her so damn much.Even though she cut my nipples.I want to suckle at her motherly bosom, slurping that rich juche milk from her teat as she gently strokes my raging erection. I would stir her velvety Korean cream into my coffee and let my balls boil in it. Her cries of pleasure and the rocking of our bed would be louder than the cacophony of ten thousand drone strikes. I would make love to her until my body gave out, and then some. I would let her break my rib cage with any part of her body. I would let her hit me with her car just to be near her for a brief moment.She's so damn perfect it hurts. Every moment without her I suffer a pain worse than breaking every bone in my body simultaneously while drowning and also having shards of glass coated in hot sauce forced through every orifice of my body. I want her, I need her. I want to desecrate her crisp general suit. I don't want her to be alone, I want to remove her mask and make her smile like the angel she is. I want to start a family with her and retire after our twenty seven children have grown up and moved out. I want to see those luscious lips speak such filthy, perverse words into my ear while she slides ice cubes down my gaping pisshole.I want to fuck her like she owes me money. I would let her step on me, just to feel the soft, firm warmth of her feet upon my face and groin area. Even slice off my nipples with her sleek cutting edge katana. I would sleep under her just to catch her drool in my mouth.`;
 }
 
 function oncBtn10() {
-    var namaWaifu = inputan();
+    let namaWaifu = inputan();
     
-    var hasilWaifu = document.getElementById('hasilWaifu');
+    let hasilWaifu = document.getElementById('hasilWaifu');
     hasilWaifu.innerHTML = `Go ahead, call me lonely, a simp, or horny. These are MY FUCKING FEELINGS. I want to have the roughest and hottest sex with ${namaWaifu}. Seriously. Her cute accent, her lovely face, and her supple body drive me to horny madness. Every inch of her would be massaged and licked. When I want to finish and climax with all of my love, I would do it on her stomach, face, tits, and roll her over on her ass to blow the last of my load on those supple cheeks. I want to cuddle with her when I am done busting, and ask her how her day was, feeling each other's warmth on our naked, vulnerable bodies. She'd tell me how good it was and she'd confide her truest feelings to me, telling me how much she loves me. I would tell her I love her back, and she would give me a loving peck on my cheeks. Then we would get dressed and spend the day watching the Sopranos, still cuddling and even eating our favorite foods. I want ${namaWaifu} to be my girlfriend, my lover, my wife, and my life. I love her and want her to be mine. Is this a copy-pasta? No. I typed out every word to proclaim how I feel. Every time you ask who she is, look back on this: SHE IS MY WIFE. I love ${namaWaifu}, and these are MY original thoughts and feelings.`;
 }
 
 function oncBtn11() {
-    var namaWaifu = inputan();
+    let namaWaifu = inputan();
     
-    var hasilWaifu = document.getElementById('hasilWaifu');
+    let hasilWaifu = document.getElementById('hasilWaifu');
     hasilWaifu.innerHTML = `I NEED TO BREED ${namaWaifu} I NEED HER TO MOTHER MY CHILDREN I NEED TO FILL HER WOMB I WANT EVERY SINGLE DROP EVERY LAST SINGLE SPERM CELL MY BODY EVER PRODUCES TO BE IN ${namaWaifu}'S WOMB TWINS TRIPLETS QUADRUPLETS I WILL SINGLE HANDEDLY FIX EUROPE'S DECLINING BIRTHRATES WITH MY WIFE ${namaWaifu} WE WILL BREED THE FOURTH REICH INTO EXISTENCE TOGETHER`;
 }
 
 function oncBtn12() {
-    var namaWaifu = inputan();
+    let namaWaifu = inputan();
     
-    var hasilWaifu = document.getElementById('hasilWaifu');
+    let hasilWaifu = document.getElementById('hasilWaifu');
     hasilWaifu.innerHTML = `EVEN BETTER, I'M INTO THAT C'MON NOW ${namaWaifu}, BRATTY MOMMY, TIME FOR SOME MUCH NEEDED CORRECTION FOR NOT USING AN ANTICONCEPTIONAL PILL💢💢💢💢💢💢 PLAP PLAP PLAP💦💦💦 PLAP PLAP PLAP💦💦💦 PLAP PLAP PLAP💦💦💦 PLAP PLAP PLAP💦💦💦 PLAP PLAP PLAP💦💦💦 PLAP PLAP PLAP💦💦💦 PLAP PLAP PLAP💦💦💦 PLAP PLAP PLAP💦💦💦 PLAP PLAP PLAP💦💦💦 PLAP PLAP PLAP💦💦💦 UUUUUUOOOOOOOHHHHHHH!!!!!! ${namaWaifu}-SAMAAA I'M GOING TO CUM💦💦💦💢💢💢💢 *ejaculation sound ejaculation sound* HUFF PUFF HUFF PUFF ~EVEN WHEN CARRYING A BABY YOU CAN STILL HANDLE IT VERY ROUGHLY~ ~WELL LET'S SEE IF YOU CAN HANDLE A ROUND TWO~ PLAP PLAP PLAP💦💦💦 PLAP PLAP PLAP💦💦💦 PLAP PLAP PLAP💦💦💦 PLAP PLAP PLAP💦💦💦 PLAP PLAP PLAP💦💦💦 PLAP PLAP PLAP💦💦💦 AAAAAAIIIIIIIIIIIIHHHHH I GOT CORRECTED A SECOND TIME >\\<💢💢💢💢 UUUUOOOOHHHHH!!!!!!!!! 💦💦💦💦💦💢💢💢💢 *ejaculation sound ejaculation sound* ${namaWaifu} CORRECTED AGAIN💢💢💢`;
 }
 
@@ -166,5 +167,5 @@ function updateNamaWaifu() {
     }
 }
 
-var waifuInput = document.getElementById('namaInput');
+let waifuInput = document.getElementById('namaInput');
 waifuInput.addEventListener('input', updateNamaWaifu);
